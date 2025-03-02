3aa0c827bfb153db27054c69ebe29a2708c37794
ntrofim20 <ntrofim35@mail.ru>
2025-03-03 00:41:16 +0500

    Hello Kitty

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-//        System.out.println("Hello World!"); // получилось без опечатки
+        System.out.println("Hello Kitty!");
     }
 }
